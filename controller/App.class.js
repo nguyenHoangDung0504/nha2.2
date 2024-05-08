cd2c2f2b79d6648ce793e827ec40663e76cde82f
nguyenHoangDung0504 <127297094+nguyenHoangDung0504@users.noreply.github.com>
2024-05-08 14:34:30 +0000

    8/5/2024 9:34 - Test format code

--- controller/App.class.js
+++ controller/App.class.js
@@ -3,30 +3,52 @@
 class App {
     static buildApp() {
         console.time('Home - Build app time');
+        App.buildHeaderAction();
+        App.buildMenuAction();
+        App.buildCategoriesModalView();
+        App.buildCategoriesModalAction();
+        App.startSendAppStatus();
+        console.timeEnd('Home - Build app time');
+    }
 
-        const [
-            openCategoryModalBtn, categoriesModal, openMenuBtn, 
-            rankListCvCtn, rankListTagCtn, rankListSeriesCtn
-        ] = [
-            '.open-category-modal-btn', '#categories-modal', '#open-menu-btn',
-            '.rank-list-grid-item.cv-b', '.rank-list-grid-item.tag-b', '.rank-list-grid-item.series-b'
-        ].map(selector => {
-            return document.querySelector(selector);
-        });
+    static buildHeaderAction() {
+        // Search action
+    }
 
-        openCategoryModalBtn.addEventListener('click', () => {
-            categoriesModal.classList.add('open');
-        });
+    static buildMenuAction() {
+        const toggleMenuBtn = document.querySelector('toggle-menu-btn');
+        const closeMenuBtn = document.querySelector('#close-menu-btn');
 
-        openMenuBtn.addEventListener('click', () => {
-            Config.toggleMenu();
-        });
-        
-        categoriesModal.querySelector('.close-btn').addEventListener('click', () => {
-            categoriesModal.classList.remove('open');
-        });
+        toggleMenuBtn?.addEventListener('click', Config.toggleMenu);
+        closeMenuBtn?.addEventListener('click', Config.closeMenu);
+    }
 
-        const accordions = document.querySelectorAll('.accordion-header');
+    // For categories modal
+    static buildCategoriesModalView() {
+        const [rankListCvCtn, rankListTagCtn, rankListSeriesCtn] = ['.rank-list-grid-item.cv-b', '.rank-list-grid-item.tag-b', '.rank-list-grid-item.series-b'].map(selector => document.querySelector(selector));
+        const [listCvCtn, listTagCtn, listSeriesCtn] = [rankListCvCtn, rankListTagCtn, rankListSeriesCtn].map(ctn => ctn.querySelector('.links'));
+        const listLength = [Database.listCv, Database.listTag, Database.listSeries].map(list => list.length);
+
+        [listCvCtn, listTagCtn, listSeriesCtn]
+            .forEach(ctn => ctn.innerHTML = '');
+        [rankListCvCtn, rankListTagCtn, rankListSeriesCtn]
+            .forEach((ctn, index) => ctn.querySelector('.title').textContent += ` (${listLength[index]})`);
+        Database.listCv.forEach(({ name, quantity }) => {
+            listCvCtn.innerHTML += `<a href="../?cv=${encodeURIComponent(name)}" class="item" quantity="${quantity}">${name}</a>`;
+        });
+        Database.listTag.forEach(({ name, quantity }) => {
+            listTagCtn.innerHTML += `<a href="../?tag=${encodeURIComponent(name)}" class="item" quantity="${quantity}">${name}</a>`;
+        });
+        Database.listSeries.forEach(({ name, quantity }) => {
+            listSeriesCtn.innerHTML += `<a href="../?series=${encodeURIComponent(name)}" class="item" quantity="${quantity}">${name}</a>`;
+        });
+    }
+    static buildCategoriesModalAction() {
+        const categoriesModal = document.querySelector('#categories-modal');
+        const accordions = categoriesModal.querySelectorAll('.accordion-header');
+        const btnOpenCategoryModal = document.querySelector('#open-categories-modal-btn');
+        const btnCloseCategoryModal = categoriesModal.querySelector('#close-categories-modal-btn');
+
         accordions.forEach(function (accordion) {
             accordion.addEventListener('click', function () {
                 this.classList.toggle('active');
@@ -39,30 +61,13 @@ class App {
             });
         });
 
-        const [listCvCtn, listTagCtn, listSeriesCtn] = [rankListCvCtn, rankListTagCtn, rankListSeriesCtn].map(ctn => ctn.querySelector('.links'));
-        [listCvCtn, listTagCtn, listSeriesCtn].forEach(ctn => ctn.innerHTML = '');
-        const listLength = [Database.listCv, Database.listTag, Database.listSeries].map(list => list.length);
-        [rankListCvCtn, rankListTagCtn, rankListSeriesCtn].forEach((ctn, index) => ctn.querySelector('.title').textContent += ` (${listLength[index]})`);
-        Database.listCv.forEach(({ name, quantity }) => {
-            listCvCtn.innerHTML += `<a href="../?cv=${encodeURIComponent(name)}" class="item" quantity="${quantity}">${name}</a>`;
-        });
-        Database.listTag.forEach(({ name, quantity }) => {
-            listTagCtn.innerHTML += `<a href="../?tag=${encodeURIComponent(name)}" class="item" quantity="${quantity}">${name}</a>`;
-        });
-        Database.listSeries.forEach(({ name, quantity }) => {
-            listSeriesCtn.innerHTML += `<a href="../?series=${encodeURIComponent(name)}" class="item" quantity="${quantity}">${name}</a>`;
+        btnOpenCategoryModal.addEventListener('click', () => {
+            categoriesModal.classList.add('open');
         });
 
-        App.startSendAppStatus();
-        console.timeEnd('Home - Build app time');
-    }
-
-    static buildHeader() {
-
-    }
-
-    static buildMenu() {
-
+        btnCloseCategoryModal.addEventListener('click', () => {
+            categoriesModal.classList.remove('open');
+        });
     }
 
     static startSendAppStatus() {

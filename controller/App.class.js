003f77590b2c2eebeb54f6118727df6e4e482289
nguyenHoangDung0504 <127297094+nguyenHoangDung0504@users.noreply.github.com>
2024-05-08 15:32:35 +0000

    ---d---d---

--- controller/App.class.js
+++ controller/App.class.js
@@ -1,23 +1,43 @@
 'use strict';
 
 class App {
-    static buildApp() {
-        console.time('Home - Build app time');
-        App.buildHeaderAction();
-        App.buildMenuAction();
-        App.buildCategoriesModalView();
-        App.buildCategoriesModalAction();
-        App.startSendAppStatus();
-        console.timeEnd('Home - Build app time');
+    static types = {
+        HOME_PAGE: 'Home page',
+        WATCH_PAGE: 'Watch page',
+        ALT_PLAYER_PAGE: 'Alt player type'
     }
 
+    static buildApp(type = App.types.HOME_PAGE) {
+        console.time(`${type} - Build app time`);
+        // Common Build
+        App.buildHeaderAction();
+        App.buildMenuAction();
+
+        switch (type) {
+            case App.types.HOME_PAGE:
+                App.buildCategoriesModalView();
+                App.buildCategoriesModalAction();
+                break;
+            case App.types.WATCH_PAGE:
+                break;
+            case App.types.ALT_PLAYER_PAGE:
+                break;
+            default: throw new Error('Invalid app type');
+        }
+
+        App.startSendAppStatus();
+        console.timeEnd(`${type} - Build app time`);
+    }
+
+    // For search box
     static buildHeaderAction() {
         // Search action
     }
 
+    // For menu
     static buildMenuAction() {
-        const toggleMenuBtn = document.querySelector('toggle-menu-btn');
-        const closeMenuBtn = document.querySelector('#close-menu-btn');
+        const toggleMenuBtn = document.querySelector('#toggle-menu-btn') ?? (App.warnSelectorNotFound)();
+        const closeMenuBtn = document.querySelector('#close-menu-btn') ?? (App.warnSelectorNotFound)();
 
         toggleMenuBtn?.addEventListener('click', Config.toggleMenu);
         closeMenuBtn?.addEventListener('click', Config.closeMenu);
@@ -25,34 +45,36 @@ class App {
 
     // For categories modal
     static buildCategoriesModalView() {
-        const [rankListCvCtn, rankListTagCtn, rankListSeriesCtn] = ['.rank-list-grid-item.cv-b', '.rank-list-grid-item.tag-b', '.rank-list-grid-item.series-b'].map(selector => document.querySelector(selector));
-        const [listCvCtn, listTagCtn, listSeriesCtn] = [rankListCvCtn, rankListTagCtn, rankListSeriesCtn].map(ctn => ctn.querySelector('.links'));
+        const [rankListCvCtn, rankListTagCtn, rankListSeriesCtn] = ['.rank-list-grid-item.cv-b', '.rank-list-grid-item.tag-b', '.rank-list-grid-item.series-b'].map(selector => document.querySelector(selector) ?? (App.warnSelectorNotFound)());
+        const [listCvCtn, listTagCtn, listSeriesCtn] = [rankListCvCtn, rankListTagCtn, rankListSeriesCtn].map(ctn => ctn.querySelector('.links') ?? (App.warnSelectorNotFound)());
         const listLength = [Database.listCv, Database.listTag, Database.listSeries].map(list => list.length);
+        let [cvHtml, tagHtml, seriesHtml] = Array(3).fill('');
 
-        [listCvCtn, listTagCtn, listSeriesCtn]
-            .forEach(ctn => ctn.innerHTML = '');
         [rankListCvCtn, rankListTagCtn, rankListSeriesCtn]
             .forEach((ctn, index) => ctn.querySelector('.title').textContent += ` (${listLength[index]})`);
         Database.listCv.forEach(({ name, quantity }) => {
-            listCvCtn.innerHTML += `<a href="../?cv=${encodeURIComponent(name)}" class="item" quantity="${quantity}">${name}</a>`;
+            cvHtml += `<a href="../?cv=${encodeURIComponent(name)}" class="item" quantity="${quantity}">${name}</a>`;
         });
         Database.listTag.forEach(({ name, quantity }) => {
-            listTagCtn.innerHTML += `<a href="../?tag=${encodeURIComponent(name)}" class="item" quantity="${quantity}">${name}</a>`;
+            tagHtml += `<a href="../?tag=${encodeURIComponent(name)}" class="item" quantity="${quantity}">${name}</a>`;
         });
         Database.listSeries.forEach(({ name, quantity }) => {
-            listSeriesCtn.innerHTML += `<a href="../?series=${encodeURIComponent(name)}" class="item" quantity="${quantity}">${name}</a>`;
+            seriesHtml += `<a href="../?series=${encodeURIComponent(name)}" class="item" quantity="${quantity}">${name}</a>`;
         });
+        listCvCtn.innerHTML = cvHtml;
+        listTagCtn.innerHTML = tagHtml;
+        listSeriesCtn.innerHTML = seriesHtml;
     }
     static buildCategoriesModalAction() {
-        const categoriesModal = document.querySelector('#categories-modal');
-        const accordions = categoriesModal.querySelectorAll('.accordion-header');
-        const btnOpenCategoryModal = document.querySelector('#open-categories-modal-btn');
-        const btnCloseCategoryModal = categoriesModal.querySelector('#close-categories-modal-btn');
+        const categoriesModal = document.querySelector('#categories-modal') ?? (App.warnSelectorNotFound)();
+        const accordions = categoriesModal.querySelectorAll('.accordion-header') ?? (App.warnSelectorNotFound)();
+        const btnOpenCategoryModal = document.querySelector('#open-categories-modal-btn') ?? (App.warnSelectorNotFound)();
+        const btnCloseCategoryModal = categoriesModal.querySelector('#close-categories-modal-btn') ?? (App.warnSelectorNotFound)();
 
-        accordions.forEach(function (accordion) {
-            accordion.addEventListener('click', function () {
-                this.classList.toggle('active');
-                let panel = this.nextElementSibling;
+        accordions?.forEach(accordion => {
+            accordion.addEventListener('click', () => {
+                accordion.classList.toggle('active');
+                let panel = accordion.nextElementSibling;
                 if (panel.style.maxHeight) {
                     panel.style.maxHeight = null;
                 } else {
@@ -61,21 +83,27 @@ class App {
             });
         });
 
-        btnOpenCategoryModal.addEventListener('click', () => {
-            categoriesModal.classList.add('open');
+        btnOpenCategoryModal?.addEventListener('click', () => {
+            categoriesModal?.classList.add('open');
         });
 
-        btnCloseCategoryModal.addEventListener('click', () => {
-            categoriesModal.classList.remove('open');
+        btnCloseCategoryModal?.addEventListener('click', () => {
+            categoriesModal?.classList.remove('open');
         });
     }
 
+    // Call when complete build app
     static startSendAppStatus() {
         // Send status to the app that embeds this app
         parent.postMessage({ type: 'urlChange', version: 2.2, url: window.location.href }, '*');
         setInterval(() => parent.postMessage({ type: 'alive' }, '*'), 2500);
         window.addEventListener('beforeunload', () => parent.postMessage({ type: 'beforeUnload' }, '*'));
     }
-}
 
-App.buildApp();
+    static warnSelectorNotFound() {
+        const stackTrace = new Error().stack;
+        const callingLine = stackTrace.split('\n')[2];
+        console.warn(`Warning, selector not found ${callingLine.trim()}`);
+        return false;
+    }
+}

ef80342d01ad368999c7af0a45b8a225b290639a
nguyenHoangDung0504 <127297094+nguyenHoangDung0504@users.noreply.github.com>
2024-05-13 14:49:10 +0000

    complete download function

--- models/classes.js
+++ models/classes.js
@@ -490,7 +490,7 @@ class AudioController {
         this.currentTime = 0;
         this.touchStartX = 0;
         this.time = 3;
-        this.filename = Utils.getFileName(src);
+        this.filename = Utils.getFileNameFromUrl(src);
 
         this.audContainer = document.createElement('div');
         this.audio = document.createElement('audio');
